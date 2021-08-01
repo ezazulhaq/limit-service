ad8c872e1053bd9322f563a9cb0ca96e2a711c15
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-08-01 18:38:51 +0530

    configure limits using properties file

--- src/main/java/com/haa/limitservice/controller/LimitsController.java
+++ src/main/java/com/haa/limitservice/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.haa.limitservice.controller;
 
 import com.haa.limitservice.bean.Limits;
+import com.haa.limitservice.configuration.LimitConfiguration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private LimitConfiguration limitConfiguration;
+
     @GetMapping(path = "/limits")
     public Limits retriveLimits() {
-        return new Limits(1, 1000);
+        // return new Limits(1, 1000);
+        return new Limits(limitConfiguration.getMinimum(), limitConfiguration.getMaximum());
     }
 }
